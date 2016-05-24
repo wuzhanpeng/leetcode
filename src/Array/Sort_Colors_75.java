184702741c8f34662b658a83368d5e68486fba1a
wuzhanpeng <zhanpeng.wu@qq.com>
2016-05-24 16:03:05 +0800

    2016/05/24 16:03:05

---
+++ src/Array/Sort_Colors_75.java
@@ -0,0 +1,27 @@
+package Array;
+
+public class Sort_Colors_75 {
+	
+	public class Solution {
+	    public void sortColors(int[] nums) {
+	        int l = 0, r = nums.length - 1;
+	        for (int i = 0; i <= r;) {
+	        	if (nums[i] == 0) {
+	        		nums[i ++] = nums[l];
+	        		nums[l ++] = 0;
+	        	} else if (nums[i] == 2) {
+	        		nums[i] = nums[r];
+	        		nums[r --] = 2;
+	        	} else {
+					++ i;
+				}
+	        }
+	    }
+	}
+
+	public static void main(String[] args) {
+		new Sort_Colors_75().new Solution().sortColors(new int[]{0,1,2,0,2});
+		new Sort_Colors_75().new Solution().sortColors(new int[]{2,1,0,2,0});
+	}
+
+}
