a4eae77a8f3b9ac4b4fc22f0d0990538880dfc00
wuzhanpeng <zhanpeng.wu@qq.com>
2016-05-26 09:49:08 +0800

    2016/05/26 09:49:08

--- src/Math/Super_Ugly_Number_313.java
+++ src/Math/Super_Ugly_Number_313.java
@@ -1,27 +1,49 @@
 package Math;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 import leetcode.util.Debug;
 
 public class Super_Ugly_Number_313 {
 	
+	// for special case, n could not reach 10^6
+	// such as 1800 [2,3,5]
 	public class Solution {
+		public class Prime {
+			public int index;
+			public int prime;
+			public int val;
+			public Prime(int index, int prime, int val) {
+				this.index = index;
+				this.prime = prime;
+				this.val = val;
+			}
+		}
 	    public int nthSuperUglyNumber(int n, int[] primes) {
-	        long[] uglySeq = new long[n];
+	        int[] uglySeq = new int[n];
 	        uglySeq[0] = 1;
-	        int idx = 1;
-	        PriorityQueue<Long> queue = new PriorityQueue<Long>();
-	        for (int prime : primes) { queue.add((long) prime); }
-	        while (idx < n) {
-	        	long min = queue.poll();
-	        	if (min == uglySeq[idx - 1]) { continue; }
-	        	uglySeq[idx ++] = (int) min;
-	        	for (int i = 0; i < primes.length; ++ i) {
-	        		if (min * primes[i] <= 0) { break; } 
-	        		queue.add(min * primes[i]);
-	        	}
+	        PriorityQueue<Prime> queue = new PriorityQueue<>(new Comparator<Prime>() {
+
+				@Override
+				public int compare(Prime o1, Prime o2) {
+					return o1.val - o2.val;
+				}
+			});
+	        for (int i = 0; i < primes.length; ++ i) {
+        		Prime prime = new Prime(0, primes[i], primes[i]);
+        		queue.add(prime);
+        	}
+	        for (int idx = 1; idx < n; ++ idx) {
+	        	do {					
+		        	Prime prime = queue.poll();
+		        	uglySeq[idx] = prime.val;
+		        	prime.val = uglySeq[++ prime.index] * prime.prime;
+		        	queue.add(prime);
+				} while (uglySeq[idx] == queue.peek().val);
+		        System.out.println(uglySeq[idx]);
 	        }
+	        
 	        Debug.printArray(uglySeq);
 	        System.out.println(uglySeq[n - 1]);
 	        return (int) uglySeq[n - 1];
@@ -29,7 +51,7 @@ public class Super_Ugly_Number_313 {
 	}
 
 	public static void main(String[] args) {
-		new Super_Ugly_Number_313().new Solution().nthSuperUglyNumber(3500, new int[]{2,3,5});
+		new Super_Ugly_Number_313().new Solution().nthSuperUglyNumber(1800, new int[]{2,3,5});
 	}
 
 }
