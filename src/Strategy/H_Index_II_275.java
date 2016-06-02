3680212bdd65f0513286152e9fd3c956c6660319
wuzhanpeng <zhanpeng.wu@qq.com>
2016-06-02 10:34:52 +0800

    2016/06/02 10:34:52

--- src/Strategy/H_Index_II_275.java
+++ src/Strategy/H_Index_II_275.java
@@ -4,7 +4,7 @@ public class H_Index_II_275 {
 	
 	public class Solution {
 	    public int hIndex(int[] citations) {
-	        int l = 0, r = citations.length - 1;
+	        int l = 0, r = citations.length; // here: r is (length) instead of (length - 1)
 	        while (l < r) {
 	        	int mid = (l + r) / 2;
 	        	if (citations[mid] >= citations.length - mid) { r = mid; }
