f818f280201feb1a3d2ace399e512422b2e051f7
wuzhanpeng <zhanpeng.wu@qq.com>
2016-06-04 15:45:15 +0800

    2016/06/04 15:45:15

--- src/Array/Search_in_Rotated_Sorted_Array_33.java
+++ src/Array/Search_in_Rotated_Sorted_Array_33.java
@@ -3,6 +3,26 @@ package Array;
 public class Search_in_Rotated_Sorted_Array_33 {
 	
 	public class Solution {
+		/** more clear method */
+		public class Solution2 {
+		    public int search(int[] nums, int target) {
+		    	int l = 0, r = nums.length - 1;	        
+		        while (l < r) {
+		        	int mid = (l + r) / 2;
+		        	if (target == nums[mid]) { return mid; }
+		        	if (nums[mid] > nums[l]) {
+		        		if (target >= nums[l] && target < nums[mid]) { r = mid - 1; }
+		        		else { l = mid + 1; }
+		        	} else if (nums[mid] < nums[l]) {
+		        		if (target > nums[mid] && target <= nums[r]) { l = mid + 1; }
+		        		else { r = mid - 1; }
+		        	} else { ++ l; }
+		        }
+		        return target == nums[l] ? l : -1;
+		    }
+		}
+		
+		/** origin */
 		// ascending
 	    public int search(int[] nums, int target) {
 	        int l = 0, r = nums.length - 1, max = -1;
