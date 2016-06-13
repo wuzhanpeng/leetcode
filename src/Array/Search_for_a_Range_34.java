9ec46ef34d4b4f154e16248b4221357b5fe1b7a7
wuzhanpeng <zhanpeng.wu@qq.com>
2016-06-13 14:43:12 +0800

    2016/06/13 14:43:12

---
+++ src/Array/Search_for_a_Range_34.java
@@ -0,0 +1,33 @@
+package Array;
+
+public class Search_for_a_Range_34 {
+	
+	public class Solution {
+	    public int[] searchRange(int[] nums, int target) {
+	    	int mid = 0, l = 0, r = nums.length - 1;
+	        while (l < r) {
+	        	mid = (l + r) / 2;
+	        	if (target < nums[mid]) {
+	        		r = mid;
+	        	} else if (target > nums[mid]) {
+					l = mid + 1;
+				} else {
+					break;
+				}
+	        }
+	        if (target == nums[mid]) {
+	        	int beg = mid, end = mid;
+	        	while (beg > 0 && target == nums[beg - 1]) { -- beg; }
+	        	while (end < nums.length - 1 && target == nums[end + 1]) { ++ end; }
+	        	return new int[]{beg, end};
+	        } else {
+				return new int[]{-1, -1};
+			}
+	    }
+	}
+
+	public static void main(String[] args) {
+		new Search_for_a_Range_34().new Solution().searchRange(new int[]{1,2}, 2);
+	}
+
+}
