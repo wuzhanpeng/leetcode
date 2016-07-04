f88b748fee92f9300e06d67de084f5df734355ac
wuzhanpeng <zhanpeng.wu@qq.com>
2016-07-04 18:46:53 +0800

    2016/07/04 18:46:53

--- src/Graph/Course_Schedule_207.java
+++ src/Graph/Course_Schedule_207.java
@@ -1,36 +1,51 @@
 package Graph;
 
-import java.util.Arrays;
-import java.util.Comparator;
-
-import leetcode.util.Debug;
+import java.util.LinkedList;
 
 public class Course_Schedule_207 {
 	
 	public class Solution {
+		private boolean Kahn(int numVertices, int[][] edges) {
+			boolean[][] adjacencyMatrix = new boolean[numVertices][numVertices];
+			int restEdges = 0;
+			// 0 degree vertex
+			int[] inDegree = new int[numVertices];
+			for (int i = 0; i < edges.length; ++ i) {
+				if (!adjacencyMatrix[edges[i][1]][edges[i][0]]) {
+					++ restEdges; ++ inDegree[edges[i][0]];
+					adjacencyMatrix[edges[i][1]][edges[i][0]] = true;
+				}
+			}
+			LinkedList<Integer> vertices = new LinkedList<>();
+			for (int i = 0; i < inDegree.length; ++ i) {
+				if (0 == inDegree[i]) { vertices.add(i); }
+			}
+			while (!vertices.isEmpty()) {
+				LinkedList<Integer> newAdded = new LinkedList<>();
+				while (!vertices.isEmpty()) {
+					int vertex = vertices.poll();
+					for (int pointed = 0; pointed < numVertices; ++ pointed) {
+						if (adjacencyMatrix[vertex][pointed]) {
+							adjacencyMatrix[vertex][pointed] = false;
+							-- restEdges; -- inDegree[pointed];
+							if (0 == inDegree[pointed]) {
+								newAdded.add(pointed);
+							}
+						}
+					}
+				}
+				vertices.addAll(newAdded);
+			}
+			return 0 == restEdges;
+		}
 	    public boolean canFinish(int numCourses, int[][] prerequisites) {
-	        boolean[] hasFinished = new boolean[numCourses];
-	        for (int i = prerequisites.length - 1; i >= 0; -- i) {
-	        	for (int j = prerequisites[0].length - 1; j >= 0; -- j) {
-	        		if (hasFinished[prerequisites[i][j]]) { return false; }
-	        		hasFinished[prerequisites[i][j]] = true;
-	        	}
-	        }
-	        return true;
+	        return Kahn(numCourses, prerequisites);
 	    }
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[][] s = new int[][]{{1,0},{2,0},{1,2}};
-		Arrays.sort(s, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				// TODO Auto-generated method stub
-				return o1[1] - o2[1];
-			}
-		});
-		Debug.printMatrix(s);
+		new Course_Schedule_207().new Solution().canFinish(10, 
+				new int[][]{{5,8},{3,5},{1,9},{4,5},{0,2},{1,9},{7,8},{4,9}});
 	}
 
 }
